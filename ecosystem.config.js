bcd1264e23e287483982e62300cadcce14dc5d66
comercial trujillo <victoryfullpower@gmail.com>
2025-08-04 20:33:18 -0500

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [{
+    name: "mi-nextjs",
+    script: "node_modules/next/dist/bin/next",
+    args: "start",
+    cwd: "D:/SistemaCCP_web/cpprimavera", // Usa barras normales /
+    interpreter: "node"
+  }]
+}
