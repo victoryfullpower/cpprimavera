24c4f6d862ed8c58ff7af634d772fb3237dbdec1
victor <victoryfullpower@gmail.com>
2025-08-05 18:33:37 -0500

    eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   apps: [{
-    name: "mi-nextjs",
+    name: "NextJS",
     script: "node_modules/next/dist/bin/next",
     args: "start",
-    cwd: "D:/SistemaCCP_web/cpprimavera", // Usa barras normales /
-    interpreter: "node"
+    cwd: "D:/SistemaCCP_web/cpprimavera",  // Usa / en lugar de \
+    interpreter: "node",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "1G",
+    env: {
+      NODE_ENV: "production"
+    }
   }]
 }
